301a0583098f1dfdee4bcbb993a96726a1b811b9
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-06-26 11:08:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -2,17 +2,17 @@ class Solution {
   public:
     int findUnion(vector<int>& a, vector<int>& b) {
         // code here
-        map<int,int>mp;
-        int n=a.size();
-        int m=b.size();
-        for(int i=0;i<n;i++)
-        {
+       unordered_map<int,int>mp;
+       int n=a.size();
+       int m=b.size();
+       for(int i=0;i<n;i++)
+       {
            mp[a[i]]++;
-        }
+       }
         for(int i=0;i<m;i++)
-        {
+       {
            mp[b[i]]++;
-        }
-        return mp.size();
+       }
+       return (mp.size());
     }
 };
