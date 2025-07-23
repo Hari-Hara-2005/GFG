42cece39ec446742a95f63f81d13316541267a68
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-07-23 19:32:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Run Length Encoding/run-length-encoding.cpp
+++ Difficulty: Easy/Run Length Encoding/run-length-encoding.cpp
@@ -3,23 +3,18 @@ class Solution {
     string encode(string s) {
         // code here
     int n=s.size();
-    char ch=s[0];
-    int cnt=0;
-    string result="";
-    for(int i=0;i<n;i++)
-    {
-        if(s[i]==ch){
+    string res="";
+    int i=0;
+    while(i<n){
+        int cnt=0;
+        char ch=s[i];
+        while(i<n && s[i]==ch){
             cnt++;
+            i++;
         }
-        else{
-            result+=ch;
-            result+=to_string(cnt);
-            ch=s[i];
-            cnt=1;
-        }
+        res+=ch;
+        res+=to_string(cnt);
     }
-    result+=ch;
-    result+=to_string(cnt);
-    return result;
+    return res;
     }
 };
