40bb87701057d3a61b54f956baa91a149ad6344a
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-08-09 20:55:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -2,8 +2,8 @@ class Solution {
   public:
     int countFreq(vector<int>& arr, int target) {
         // code here
+        int n = arr.size();
         int cnt=0;
-        int n=arr.size();
         for(int i=0;i<n;i++)
         {
             if(arr[i]==target)
