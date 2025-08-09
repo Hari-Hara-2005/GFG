bb0a83a3b901115117418645441572ae2ed802ce
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-08-09 21:40:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Searching in an Array/searching-in-an-array.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    int search(int k, vector<int>& arr) {
+        // code here
+       int n=arr.size();
+       for(int i=0;i<n;i++)
+       {
+           if(arr[i]==k)
+           {
+               return i+1;
+           }
+       }
+       return -1;
+    }
+};
