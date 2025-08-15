cc59f244d0ec27527ba921b82822bce9098a4dcb
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-08-15 09:21:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -2,36 +2,30 @@ class Solution {
   public:
     void sort012(vector<int>& arr) {
         // code here
-        int cnt0 = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int n = arr.size();
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i] == 0)
-            {
-                cnt0++;
-            }
-            else if(arr[i] == 1)
-            {
-                cnt1++;
-            }
-            else{
-                cnt2++;
-            }
-        }
-        int idx=0;
-        for(int i=0;i<cnt0;i++)
-        {
-            arr[idx++] = 0;
-        }
-        for(int i=0;i<cnt1;i++)
-        {
-            arr[idx++] =1;
-        }
-        for(int i=0;i<cnt2;i++)
-        {
-            arr[idx++] =2;
-        }
+       int n = arr.size();
+       int c0=0,c1=0,c2=0;
+       for(int i=0;i<n;i++)
+       {
+           if(arr[i] == 0)c0++;
+           else if(arr[i] == 1)c1++;
+           else c2++;
+       }
+       for(int i=0;i<n;i++)
+       {
+           if(c0>0)
+           {
+               arr[i]=0;
+               c0--;
+           }
+           else if(c1>0)
+           {
+               arr[i]=1;
+               c1--;
+           }
+           else {
+               arr[i]=2;
+               c2--;
+           }
+       }
     }
 };
