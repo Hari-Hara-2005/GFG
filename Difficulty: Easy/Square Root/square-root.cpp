80041b480f9f51e0cabb93ae3dc5b82a47231446
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-08-27 21:44:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // code here
+        int ans=1;
+        for(int i=1;i<=n;i++)
+        {
+            if(i*i<=n)
+            {
+                ans=i;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
