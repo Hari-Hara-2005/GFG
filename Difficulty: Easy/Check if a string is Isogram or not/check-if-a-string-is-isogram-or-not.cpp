43a33a63443febe3d4347d712e47a02f05069967
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-09-10 21:41:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if a string is Isogram or not/check-if-a-string-is-isogram-or-not.cpp
+++ Difficulty: Easy/Check if a string is Isogram or not/check-if-a-string-is-isogram-or-not.cpp
@@ -2,15 +2,12 @@ class Solution {
   public:
     bool isIsogram(string& s) {
         //  code here
-        vector<int>ans(26,0);
-        int n =s.size();
+        unordered_map<char,int>mp;
+        int n = s.size();
         for(int i=0;i<n;i++)
         {
-            ans[s[i]-'a']++;
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(ans[s[i]-'a']>=2)return false;
+            mp[s[i]]++;
+            if(mp[s[i]] >=2)return false;
         }
         return true;
     }
