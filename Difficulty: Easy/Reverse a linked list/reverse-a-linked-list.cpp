3665e9a3c4bebcfb05820515f1aef04540cd7a84
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-03 10:43:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.cpp
@@ -0,0 +1,29 @@
+/*
+class Node {
+ public:
+    int data ;
+    Node *next ;
+
+    Node(int x) {
+        data = x ;
+        next = NULL ;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* reverseList(Node* head) {
+        // code here
+        Node* curr = head;
+        Node* prev = nullptr;
+        while(curr)
+        {
+            Node* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
