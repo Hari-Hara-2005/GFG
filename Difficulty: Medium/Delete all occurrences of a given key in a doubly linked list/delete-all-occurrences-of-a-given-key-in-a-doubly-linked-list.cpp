db8dacf3e5d0bf628ce74f294b7eff4417cc5018
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-04 16:24:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Delete all occurrences of a given key in a doubly linked list/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list.cpp
+++ Difficulty: Medium/Delete all occurrences of a given key in a doubly linked list/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list.cpp
@@ -1,32 +1,43 @@
+// User function Template for C++
+
+/* a Node of the doubly linked list
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
 class Solution {
   public:
-    void deleteAllOccurOfX(struct Node** head, int x) {
-        if (!head || !(*head)) return; 
-
-        Node* curr = *head;
-        
-        while (curr) {
-            if (curr->data == x) {
-                Node* toDelete = curr;
-
-        
-                if (curr == *head) {
-                    *head = curr->next;
-                    if (*head) {
-                        (*head)->prev = nullptr;
-                    }
-                }
-              
-                else {
-                    if (curr->prev)
-                        curr->prev->next = curr->next;
-                    if (curr->next)
-                        curr->next->prev = curr->prev;
-                }
-
-                curr = curr->next;
-                delete toDelete;
-            } else {
+    void deleteAllOccurOfX(struct Node** head_ref, int x) {
+        // Write your code here
+        Node* curr = *head_ref;
+        while(curr)
+        {
+            if(curr->data == x)
+            {
+            Node* deleteNode = curr;
+            if(curr == *head_ref)
+            {
+                *head_ref = curr->next;
+                curr->prev = nullptr;
+            }
+            else{
+            if(curr->prev)
+            {
+                curr->prev->next = curr->next;
+            }
+            if(curr->next)
+            {
+                curr->next->prev = curr->prev;
+            }
+            }
+            curr = curr->next;
+            delete deleteNode;
+            }
+            else{
                 curr = curr->next;
             }
         }
