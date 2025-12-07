fa9626a26a88eb0d50316e3be892277b51bc60bd
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-07 14:31:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,16 +1,47 @@
 class Solution {
   public:
-    int countFreq(vector<int>& arr, int target) {
-        // code here
-        int n = arr.size();
-        int cnt=0;
-        for(int i=0;i<n;i++)
+    int lowerBound(vector<int>& arr, int target){
+        int low =0;
+        int high=arr.size()-1;
+        int ans =arr.size();
+        while(low<=high)
         {
-            if(arr[i]==target)
+            int mid = low+(high-low)/2;
+            if(arr[mid]>=target)
             {
-                cnt++;
+                ans = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid+1;
             }
         }
+        return ans;
+    }
+    int upperBound(vector<int>& arr, int target){
+        int low =0;
+        int high=arr.size()-1;
+        int ans =arr.size();
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            if(arr[mid]>target)
+            {
+                ans = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    int countFreq(vector<int>& arr, int target) {
+        // code here
+        int first = lowerBound(arr,target);
+        int last = upperBound(arr,target);
+        int cnt = last - first;
+        if(first == arr.size() || arr[first] != target)return 0;
         return cnt;
     }
 };
