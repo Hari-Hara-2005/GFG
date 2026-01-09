0c356c353f07634639b9ee1963c3a7dd507ca582
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-09 23:34:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    void rotateMatrix(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<m;j++)
+            {
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            
+            int top = 0;
+            int bottom = n-1;
+            while(top<bottom)
+            {
+                swap(mat[top][i],mat[bottom][i]);
+                top++;
+                bottom--;
+            }
+        }
+    }
+};
