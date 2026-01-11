7108c342545c864be75c1488123dd1094f7eba04
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-11 11:31:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> nthRowOfPascalTriangle(int n) {
+        // code here
+        vector<int>ans;
+        int res = 1;
+        ans.push_back(res);
+        for(int i=1;i<n;i++)
+        {
+            res = res*(n-i);
+            res = res/i;
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
