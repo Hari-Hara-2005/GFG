1f5adc4c1f5f8b847f7106e78394e85ce49716a0
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-22 10:26:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,47 +1,17 @@
 class Solution {
   public:
-    int lowerBound(vector<int>& arr, int target){
-        int low =0;
-        int high=arr.size()-1;
-        int ans =arr.size();
-        while(low<=high)
-        {
-            int mid = low+(high-low)/2;
-            if(arr[mid]>=target)
-            {
-                ans = mid;
-                high = mid -1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        return ans;
-    }
-    int upperBound(vector<int>& arr, int target){
-        int low =0;
-        int high=arr.size()-1;
-        int ans =arr.size();
-        while(low<=high)
-        {
-            int mid = low+(high-low)/2;
-            if(arr[mid]>target)
-            {
-                ans = mid;
-                high = mid -1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        return ans;
-    }
     int countFreq(vector<int>& arr, int target) {
         // code here
-        int first = lowerBound(arr,target);
-        int last = upperBound(arr,target);
-        int cnt = last - first;
-        if(first == arr.size() || arr[first] != target)return 0;
-        return cnt;
+        int n = arr.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.first == target)return it.second;
+        }
+        return 0;
     }
 };
