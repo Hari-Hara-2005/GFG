a86ff8633a239d6a23fe54b54230c68e6b73e824
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-22 10:16:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
   public:
     bool twoSum(vector<int>& arr, int target) {
-        int n=arr.size();
-        unordered_map<int,int>mp;
+        // code here
+        map<int,int>mp;
+        int n = arr.size();
         for(int i=0;i<n;i++)
         {
-            int more=target-arr[i];
+            int more = target - arr[i];
             if(mp.find(more)!=mp.end())
             {
                 return true;
             }
-            mp[arr[i]]=i;
+            mp[arr[i]] = i;
         }
         return false;
     }
