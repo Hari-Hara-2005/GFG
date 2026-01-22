421a2c40f273edc57395e029620f0e6f09227ed4
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-22 13:21:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Element with left side smaller and right side greater/element-with-left-side-smaller-and-right-side-greater.cpp
+++ Difficulty: Easy/Element with left side smaller and right side greater/element-with-left-side-smaller-and-right-side-greater.cpp
@@ -2,23 +2,20 @@ class Solution {
   public:
     int findElement(vector<int> &arr) {
         // code here
-        int n=arr.size();
-        int ans=0;
-        int maxi=0;
+        int n = arr.size();
+        int ans =-1;
+        int maxi = INT_MIN;
         for(int i=1;i<n-1;i++)
         {
             if(arr[i]>arr[i-1] && arr[i]<arr[i+1])
             {
-                ans=arr[i];
+                ans = arr[i];
             }
             else{
-                maxi=max(maxi,arr[i]);
+                maxi = max(maxi,arr[i]);
             }
         }
-        if(ans>maxi)
-        {
-            return ans;
-        }
+        if(ans>maxi)return ans;
         return -1;
     }
 };
