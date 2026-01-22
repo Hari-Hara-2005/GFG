02f9b46399911a81c44254124f4f7e9b75d97683
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-22 10:22:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.cpp
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.cpp
@@ -3,14 +3,11 @@ class Solution {
     // Function to count the frequency of all elements from 1 to N in the array.
     vector<int> frequencyCount(vector<int>& arr) {
         // code here
-        int n=arr.size();
+        int n = arr.size();
         vector<int>ans(n,0);
         for(int i=0;i<n;i++)
         {
-            if(arr[i]>=1 && arr[i]<=n)
-            {
-                ans[arr[i]-1]++;
-            }
+            ans[arr[i]-1]++;
         }
         return ans;
     }
