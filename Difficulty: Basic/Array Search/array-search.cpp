aac8600acc69469ca15e60910e45186bfabc1e39
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-06-26 22:12:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.cpp
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -5,7 +5,10 @@ class Solution {
         int n = arr.size();
         for(int i=0;i<n;i++)
         {
-            if(arr[i] == x)return i;
+            if(arr[i] == x)
+            {
+                return i;
+            }
         }
         return -1;
     }
