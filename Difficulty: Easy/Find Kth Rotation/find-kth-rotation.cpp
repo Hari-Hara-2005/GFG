867e636e011044d06fe518250bbeeae3c41b4ff3
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-07-04 10:44:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -1,32 +1,17 @@
 class Solution {
-  public:
-    int findKRotation(vector<int> &arr) {
-        // Code Here
-        int n = arr.size();
-        int low=0,high = n-1;
-        int mini = INT_MAX;
-        int ans =-1;
-        while(low<=high)
-        {
-            int mid = low +(high-low)/2;
-            if(arr[low]<=arr[mid])
-            {
-                if(arr[low]<mini)
-                {
-                    ans = low;
-                }
-                mini = min(arr[low],mini);
-                low = mid+1;
-            }
-            else{
-                if(arr[mid]<mini)
-                {
-                    ans = mid;
-                }
-                mini = min(arr[mid],mini);
-                high = mid-1;
-            }
-        }
-        return ans;
-    }
+	public:
+	int findKRotation(vector<int> &arr) {
+		// Code Here
+		int n = arr.size();
+		int cnt = 0;
+		for (int i = 0; i<n; i++)
+			{
+			if (arr[i] < arr[cnt])
+				{
+				cnt = i;
+			}
+		}
+		
+		return cnt;
+	}
 };
