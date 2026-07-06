82c53e5235c421c5275e99dcddedf722d08d2616
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-07-06 12:05:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,32 +1,47 @@
 class Solution {
-  public:
-    int kthElement(vector<int> &a, vector<int> &b, int k) {
-        // code here
-        int n = a.size();
-        int m = b.size();
-        vector<int>arr;
-        int i=0,j=0;
-        while(i<n && j<m)
-        {
-            if(a[i]<b[j])
-            {
-                arr.push_back(a[i++]);
-            }
-            else{
-                arr.push_back(b[j++]);
-            }
-        }
-        while(i<n)
-        {
-            arr.push_back(a[i++]);
-        }
-        while(j<m)
-        {
-            arr.push_back(b[j++]);
-        }
-        for(int i=0;i<arr.size();i++)
-        {
-            if(i==k-1)return arr[i];
-        }
-    }
+	public:
+	int kthElement(vector<int> &a, vector<int> &b, int k) {
+		// code here
+		int n = a.size();
+		int m = b.size();
+		int i = 0, j = 0;
+		vector<int>ans;
+		while (i<n && j<m)
+			{
+			if (a[i] <= b[j])
+				{
+				ans.push_back(a[i]);
+				i++;
+			}
+			else {
+				ans.push_back(b[j]);
+				j++;
+			}
+		}
+		while (i<n)
+			{
+			ans.push_back(a[i]);
+			i++;
+		}
+		while (j<m)
+			{
+			ans.push_back(b[j]);
+			j++;
+		}
+		
+		int low = 0, high = ans.size() - 1;
+		while (low <= high)
+			{
+			int mid = low + (high - low)/2;
+			if (mid == k - 1)return ans[mid];
+			else if (mid<k)
+				{
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+		return - 1;
+	}
 };
