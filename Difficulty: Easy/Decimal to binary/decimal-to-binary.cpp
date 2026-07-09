fad231bbbf84e5af689fd578ce7a257daca23810
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-07-09 14:27:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Decimal to binary/decimal-to-binary.cpp
+++ Difficulty: Easy/Decimal to binary/decimal-to-binary.cpp
@@ -1,21 +1,16 @@
 class Solution {
-  public:
-    string decToBinary(int n) {
-        // code here
-        string res="";
-        while(n!=1)
-        {
-            if(n%2 == 0)
-            {
-                res+='0';
-            }
-            else{
-                res+='1';
-            }
-            n = n/2;
-        }
-        res+=to_string(n);
-        reverse(res.begin(),res.end());
-        return res;
-    }
+	public:
+	string decToBinary(int n) {
+		// code here
+		string res = "";
+		while (n != 0)
+			{
+			if (n%2 == 1)res = '1' + res;
+			else {
+				res = '0' + res;
+			}
+			n = n/2;
+		}
+		return res;
+	}
 };
